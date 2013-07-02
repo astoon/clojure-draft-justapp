13c558feedf22f1c901742c7f46b1a2fc9f6d07a
Ilshad Khabibullin <astoon@spacta.com>
2013-07-02 15:58:08 +0400

    min cljs

--- cljs/justapp/core.cljs
+++ cljs/justapp/core.cljs
@@ -2,10 +2,3 @@
   (:require [clojure.browser.repl :as repl]))
 
 (repl/connect "http://localhost:9000/repl")
-
-(defn futurama
-  [value]
-  (js/alert value)
-  (js/alert "So, I'm done..."))
-
-(futurama "Welcome!")
